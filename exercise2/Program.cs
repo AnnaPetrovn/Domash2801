ecda4d9754b122a5cd4d5aef35186904d5dfedc8
Anna <annapn5@mail.ru>
2023-01-28 19:19:25 +0600

    Сделала задание к прошлому воскресенью

---
+++ exercise2/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+1, 7 -> такого числа в массиве нет*/
+
+    int[,] CreateArray(int rows, int columns)
+    {
+        int[,] array = new int[rows, columns];
+        var rnd = new Random();
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(1, 100);
+                }
+            }
+        }
+        return array;
+    }
+
+    void PrintArray(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    void FindindElementInMassiv(int[,] array, int row, int column)
+    {
+        if (row < array.GetLength(0) & column < array.GetLength(1))
+        {
+            Console.WriteLine("Искомый элемент в массиве" + array[row, column]);
+        }
+        else Console.WriteLine("Такого элемента в массиве нет");
+    }
+    
+    int[,] arr = CreateArray(4, 5);
+    PrintArray(arr);
+    FindindElementInMassiv(arr, 0, 4);
+
+
---
+++ exercise3/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    var rnd = new Random();
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = rnd.Next(1, 100);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void MiddleAriphmeticOfColumn(int[,] array)
+{
+    int middleAriphmetic = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            {
+                middleAriphmetic = middleAriphmetic + array[i, j];
+
+            }
+        }
+        Console.WriteLine(middleAriphmetic);
+        middleAriphmetic = 0;
+    }
+}
+
+int[,] arr = CreateArray(4, 5);
+PrintArray(arr);
+MiddleAriphmeticOfColumn(arr);
